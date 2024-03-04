8a04ce27ed594663b02e810868423f1c82bbc5d1
dhirajdharankar <dhir.dharankar@gmail.com>
2024-03-04 19:26:23 +0530

    Time: 2423 ms (9.25%), Space: 0B (100.00%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+
+SELECT  name FROM Employee e 
+JOIN (SELECT managerId, COUNT(managerId) AS mcount FROM Employee
+GROUP BY managerId HAVING COUNT(managerId)>4 ) d ON e.id = d.managerID
